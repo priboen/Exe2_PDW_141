69e39e3f45a5b603f772c749cddac7e211ca352e
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-06-17 21:20:50 +0700

    exe 2

---
+++ script/index.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("Video is playing");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("Audio is playing");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("Loote pictures");
+});
